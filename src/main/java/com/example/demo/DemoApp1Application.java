2152eb5f61df74649379a041e6bb7ba70e3b2f79
SumanChoudhury1 <sc850016@gmail.com>
2024-07-29 13:08:46 +0530

    java xml

---
+++ src/main/java/com/example/demo/DemoApp1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApp1Application.class, args);
+	}
+
+}
